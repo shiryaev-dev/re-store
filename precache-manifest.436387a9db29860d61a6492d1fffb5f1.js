153d46167119e3249461ae2cb53f1af611842403
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-09-01 15:51:40 +0300

    Updates

---
+++ precache-manifest.436387a9db29860d61a6492d1fffb5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cecb80830e9560cb552d8eb502f363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e965329e30cad909e0a8",
+    "url": "/static/css/main.706a2302.chunk.css"
+  },
+  {
+    "revision": "52910e790f2dbf6ffdda",
+    "url": "/static/js/2.0eae955b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.0eae955b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e965329e30cad909e0a8",
+    "url": "/static/js/main.11f91d89.chunk.js"
+  },
+  {
+    "revision": "aa03eb95e35403b45d65",
+    "url": "/static/js/runtime-main.d8a598ec.js"
+  }
+]);
